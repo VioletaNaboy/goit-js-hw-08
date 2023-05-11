03876f7a64663c5d91aa4fa291163f88a1186603
VioletaNaboy <vnaboishchykova@gmail.com>
2023-05-11 13:49:38 +0200

    gallery1

--- goit-js-hw-08/src/js/01-gallery.js
+++ goit-js-hw-08/src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-
-console.log(galleryItems);
+const elAllGallery = document.querySelector('.gallery');
+const elGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+elAllGallery.insertAdjacentHTML('afterbegin', elGallery);
+const gallery = new SimpleLightbox('.gallery .gallery__link', {
+  captions: true,
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+});
