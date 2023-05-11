01131e9bf0839305e633a5a2af5eaf9949b1cec4
VioletaNaboy <vnaboishchykova@gmail.com>
2023-05-11 16:44:15 +0200

    3

--- goit-js-hw-08/src/js/03-feedback.js
+++ goit-js-hw-08/src/js/03-feedback.js
@@ -1,1 +1,14 @@
-
+import throttle from 'lodash.throttle';
+const formInput = document.querySelector('.feedback-form');
+const emailField = document.querySelector('email');
+const messageField = document.querySelector('message');
+function saveFormState() {
+  const email = emailField.value;
+  const message = messageField.value;
+  const data = { email, message };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+formInput.addEventListener(
+  'input',
+  throttle(event => {}, 500)
+);
