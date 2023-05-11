c169addac0616fa75a2a109886d80d7eac0a113d
VioletaNaboy <vnaboishchykova@gmail.com>
2023-05-11 15:04:24 +0200

    video

--- goit-js-hw-08/src/js/02-video.js
+++ goit-js-hw-08/src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+player.on('timeupdate', function () {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(player.currentTime(), 1000)
+  );
+});
+// const savedTime = localStorage.getItem('videoplayer-current-time');
+// if (savedTime) {
+//   player.currentTime(savedTime);
+// }
