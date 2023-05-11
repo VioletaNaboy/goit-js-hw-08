f7c557b0219d4ceb87b1203f83686bc3b2fc6d94
VioletaNaboy <vnaboishchykova@gmail.com>
2023-05-11 16:21:31 +0200

    videoplayer2

--- goit-js-hw-08/src/js/02-video.js
+++ goit-js-hw-08/src/js/02-video.js
@@ -1,21 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-player.on('timeupdate', function () {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(player.currentTime(), 1000)
-  );
-});
-// const savedTime = localStorage.getItem('videoplayer-current-time');
-// if (savedTime) {
-//   player.currentTime(savedTime);
-// }
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+const storedCurrentTime = localStorage.getItem('videoplayer-current-time');
+if (storedCurrentTime !== null) {
+  player.setCurrentTime(storedCurrentTime);
+}
